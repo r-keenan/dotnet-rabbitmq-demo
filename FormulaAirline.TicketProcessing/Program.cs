74918d0b98091d394bf3f228c1c9a3b05c4a1cf8
r-keenan <ross.keenan@protonmail.com>
2024-10-14 11:41:11 -0500

    Added consumer.

--- FormulaAirline.TicketProcessing/Program.cs
+++ FormulaAirline.TicketProcessing/Program.cs
@@ -1,2 +1,34 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+Console.WriteLine("Welcome to the ticketing service");
+
+var factory = new ConnectionFactory() {
+    HostName = "localhost",
+    UserName = "user",
+    Password = "mypassword",
+    VirtualHost = "/"
+};
+
+var conn = factory.CreateConnection();
+
+using var channel = conn.CreateModel();
+
+channel.QueueDeclare("bookings", durable: true, exclusive: false, autoDelete: false, arguments: null);
+
+var consumer = new EventingBasicConsumer(channel); 
+
+consumer.Received += (model, eventArgs) => {
+    // getting byte[]
+    var body = eventArgs.Body.ToArray();
+
+    var message = Encoding.UTF8.GetString(body);
+
+    Console.WriteLine($"New ticket processing is initiated - {message}");
+};
+
+channel.BasicConsume("bookings", true, consumer);
+
+Console.ReadKey();
